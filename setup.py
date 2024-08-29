d5b9c36df6dcbf08a4e1e469c74df0a2c23a97b3
Rajat Khandelwal <rajatgupta59@gmail.com>
2024-08-29 14:11:37 +0530

    blah

--- setup.py
+++ setup.py
@@ -3,21 +3,16 @@ import sys
 
 __version__ = "0.3.0"
 
-# setup(
-#     name="prophecy-libs",
-#     version=__version__,
-#     url="https://github.com/SimpleDataLabsInc/prophecy-python-libs",
-#     packages=find_packages(exclude=["test.*", "test"]),
-#     description="Helper library for prophecy generated code",
-#     long_description=open("README.md").read(),
-#     install_requires=[],
-#     keywords=["python", "prophecy"],
-#     classifiers=[],
-#     zip_safe=False,
-#     license="GPL-3.0",
-# )
-try:
-    from semantic_release import setup_hook
-    setup_hook(sys.argv)
-except ImportError:
-    pass
+setup(
+    name="prophecy-libs",
+    version=__version__,
+    url="https://github.com/SimpleDataLabsInc/dummy",
+    packages=find_packages(exclude=["test.*", "test"]),
+    description="Helper library for prophecy generated code",
+    long_description=open("README.md").read(),
+    install_requires=[],
+    keywords=["python", "prophecy"],
+    classifiers=[],
+    zip_safe=False,
+    license="GPL-3.0",
+)
